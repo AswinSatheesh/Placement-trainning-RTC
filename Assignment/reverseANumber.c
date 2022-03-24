9f1b94d4ca0f53e610b63c7be3f390e40b7f9e68
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-03-24 07:04:05 +0530

    Assignment completed

--- Assignment/reverseANumber.c
+++ Assignment/reverseANumber.c
@@ -3,15 +3,21 @@
 #include<stdio.h>
 int main(){
     int num,reminder,reverse = 0;
+    int temp = 0;
     printf("Enter a number to reverse: ");
     scanf("%d",&num);
-
+    temp = num;
     while(num != 0){
         reminder = num % 10;
         reverse = reverse * 10 + reminder;
         num = num / 10;
     }
-    printf("Reverse of this number is : %d",reverse);  
+    printf("Reverse of this number is : %d\n",reverse);  
 
+    if(reverse == temp){
+        printf("Given number is  palindrome.\n");
+    }else{
+        printf("Not a palindrome.\n");
+    }
     return 0;
 }
