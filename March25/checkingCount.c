e54f64cddd7a9fe79a70300563102bbbeb539f59
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-03-25 20:28:20 +0530

    odd even count find pgrm added

---
+++ March25/checkingCount.c
@@ -0,0 +1,23 @@
+//In between 1 to 100 we have to find the count of even numbers and odd numbers.
+
+#include <stdio.h>
+void EvnOddCount(int n){
+    int EvnCount = 0;
+    int OddCount = 0;
+    for(int i = 1; i<=n; i++){
+        if(i % 2 == 0){
+            EvnCount++;
+        }
+        else{
+            OddCount++;
+        }
+    }
+    printf("Even number count is : %d\n", EvnCount);
+    printf("Odd number count is : %d\n",OddCount);
+}
+int main(){
+    int num;
+    printf("Enter the number :");
+    scanf("%d",&num);
+    EvnOddCount(num);
+}
