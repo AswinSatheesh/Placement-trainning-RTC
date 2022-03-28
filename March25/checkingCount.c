3b1cd27aea66d1e7b8854d8bb4ad9dc5cc9c1ddd
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-03-28 14:17:08 +0530

    code updated

--- March25/checkingCount.c
+++ March25/checkingCount.c
@@ -7,14 +7,17 @@ void EvnOddCount(int n){
     for(int i = 1; i<=n; i++){
         if(i % 2 == 0){
             EvnCount++;
+            printf("even numbers are: %d\n",i);
         }
         else{
             OddCount++;
+            printf("odd numbers are : %d\n",i);
         }
+         //printf("%d is even \n",i);
     }
     printf("Even number count is : %d\n", EvnCount);
     printf("Odd number count is : %d\n",OddCount);
-}
+} 
 int main(){
     int num;
     printf("Enter the number :");
