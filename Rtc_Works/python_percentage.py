be0ebe363231ca29976aa94547060b50d2519253
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-04-10 13:00:56 +0530

    percentage find python code added and workig perfectly

---
+++ Rtc_Works/python_percentage.py
@@ -0,0 +1,10 @@
+
+mark1 = float(input("enter  Mark1: "))
+mark2 = float(input(" enter Mark2 : "))
+mark3 = float(input("enter  Mark3: "))
+
+total = mark1 + mark2 + mark3
+# average = total / 5
+percentage = (total / 300) * 20
+
+print("Marks Percentage = %.2f"  %percentage)
