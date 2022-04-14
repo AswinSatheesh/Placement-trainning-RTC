a8b7767fa6ff4474b6712b09c054af2fb7dcf2ba
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-04-14 17:10:49 +0530

    code added

--- Cprogramming/April14/onePrintfMultiSentence.c
+++ Cprogramming/April14/onePrintfMultiSentence.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 int main(){
-    printf("This is first line \nThis is Second line \nThis is third line by using one 'printf'");
+    // printf("This is first line \nThis is Second line \nThis is third line by using one 'printf'");   //next line inside one prinf function.
+    // printf("This is for checking the tab first line \t");
+    // printf("This is second printf .");
+
+    // printf("For entering one backslash we use two backslash here \\ now you can see one backslash  , but i have used two backslash in code.");
+    printf("Iam  \"Aswin\" "); //This is for dispalying a word inside double quotes
     return 0;
 }
