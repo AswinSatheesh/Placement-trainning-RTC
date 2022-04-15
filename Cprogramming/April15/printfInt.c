9737fc0ea76e93f26289d2a8b6771ef4594ad1ad
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-04-15 11:25:58 +0530

    format specifire learned i

---
+++ Cprogramming/April15/printfInt.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int value = 10;
+    printf("%i \n",value); //For interger we can use both "%i" and "%d" format specifier. Both will generate the output.
+    printf("%d",value);
+
+    return 0;
+}
