1e78ec9fc45255c6ae624df2233361e528fc6d36
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-04-28 18:21:39 +0530

    code added

---
+++ April28/ascii.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    char alpha;
+    printf("Enter a character : \n");
+    scanf("%c",&alpha);
+    printf("Ascii value of %c is : %d",alpha,alpha);
+    return 0;
+}
