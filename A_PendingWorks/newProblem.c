a1c2819f448b3df135864d3c327487766de51bca
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-04-29 18:59:31 +0530

    new folder added prgm not completed

---
+++ A_PendingWorks/newProblem.c
@@ -0,0 +1,11 @@
+//This is a pgm need to solve.
+// input - b3x2   --- output - bbbxx
+// there are 3 b and 2 x in the question  need to write this prgm in c what ever input user enter corresponding output needs
+
+#include <stdio.h>
+int main(){
+
+    
+
+    return 0;
+}
