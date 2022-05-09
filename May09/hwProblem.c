8eaa9bb58bbded88da39e842dd516ce0d1d18d3d
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-05-09 22:56:09 +0530

    code added neet to fix

---
+++ May09/hwProblem.c
@@ -0,0 +1,27 @@
+//Write a program to count the number of repeated letter in a given word.
+// Eg : RATHINAM TECHNICAL CAMPUS   - <A ,4> <T,2> <H,2> <I,2> <N,2> <M,2> <C,3>
+
+#include<stdio.h>
+int main(){
+    char name[50] ={"RATHINAM TECHNICAL CAMPUS"};
+    int length = 0;
+    int letterCount = 0;
+    for(int i = 0; name[i]!= '\0'; i++){
+        length++;
+    }
+    printf("Length : %d \n",length);
+    printf("%s \n",name);
+    for(int j = 0; j < length; j++){
+        for(int k = 0; k < length; k++){
+            if(name[j] == name[k]){
+                letterCount++;
+            }
+        }
+        if(letterCount > 1){
+             printf("<%c, %d> \n",name[j],letterCount);
+        }
+        letterCount = 0;
+    }
+   
+    return 0;
+}
