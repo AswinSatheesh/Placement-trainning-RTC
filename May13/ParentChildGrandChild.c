e60373149e1b8374eedcb25d9fdadd1a391a728d
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-05-13 10:37:02 +0530

    code updated

--- May13/ParentChildGrandChild.c
+++ May13/ParentChildGrandChild.c
@@ -1,29 +1,25 @@
+
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 
 int main()
 {
-    char s1[4][10] = {"luke" , "wayne" , "rooney" , "shaw"};
-    char s2[4][20] = {"shaw" , "rooney" , "ronaldo" , "rooney"};
-    int count =0;
+    char list1[4][20] = {"LUKE","WAYNE","ROONEY","SHAW"};
+    char list2[4][25] = {"SHAW","ROONEY","RONALDO","ROONEY"};
+    int count = 0;
     
-    for(int i=0;i<4;i++)
-    {
-        if(strcmp(s2[i],"ronaldo")==0)
-        {
-            for(int j=0;j<4;j++)
-            {
-                if(strcmp(s1[i],s2[j])==0)
-                {
+    for(int i = 0; i < 4; i++){
+        if(strcmp(list2[i],"RONALDO") == 0){
+            
+            for(int j = 0; j < 4; j++){
+                if(strcmp(list1[i] , list2[j]) == 0){
                     count++;
                 }
             }
-            
         }
     }
     
-    printf("%d",count);
-
+    printf("%d grand children.",count);
 
     return 0;
 }
--- May13/meenaFriendsCount.c
+++ May13/meenaFriendsCount.c
@@ -1,44 +1,38 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 
 int main()
 {
-    char s1[4][10] = {"reena" , "kavi" , "kathir" , "abiga"};
-    char s2[4][20] = {"meena" , "reena" , "meena" , "isac"};
-    int count1 =0;
-    int count2 =0;
-    int count;
+    char nameList1 [4][20] = {"Reena","Kavi","Kathir","Abiga"};
+    char nameList2 [4][20] = {"Meena","Reena","Meena","Isac"};
     
-    for(int i=0;i<4;i++)
-    {
-        if(strcmp(s2[i],"meena")==0)
-        {
-            count1++;
+    int count = 0;
+
+    
+    for(int i = 0; i < 4; i++){
+        if(strcmp(nameList2[i],"Meena") == 0){
+            count++;
         }
     }
     
-    
-    
-    
-    for(int i=0;i<4;i++)
-    {
-        if(strcmp(s2[i],"meena")==0)
-        {
-            for(int j=0;j<4;j++)
-            {
-                if(strcmp(s1[i],s2[j])==0)
-                {
-                    count2++;
+    for(int j = 0; j < 4; j++){
+        if(strcmp(nameList2[j],"Meena")== 0){
+            for(int k = 0; k <4; k++){
+                if(strcmp(nameList1[j],nameList2[k]) == 0){
+                    count++;
                 }
             }
-            
         }
     }
-    
-    count=count1+count2;
-    
-    printf("%d",count);
 
-
+    printf("Meena's friends Total count = %d",count);
     return 0;
 }
