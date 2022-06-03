12b8159cd2e339c7c3e29db6e53df320e93d25e8
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-06-03 17:46:14 +0530

    code added

---
+++ JAVA-PROGRAMMING/June02/Sample.java
@@ -0,0 +1,8 @@
+public class Sample{
+    public static void main(String[] args){
+        System.out.println("This sample pgrm is working");
+        System.out.println(100 + 100); //for doing numeric operations don't need to use " " double brackets.
+        System.out.print("First line"); // here we are not using println so both of them print in a same line.
+        System.out.print("Second line");
+    }
+}
---
+++ JAVA-PROGRAMMING/june03/TypeCasting.java
@@ -0,0 +1,8 @@
+public class TypeCasting{
+    public static void main(String[] args){
+        int myint = 10;
+        double myDouble = (double) myint;
+        System.out.println(myint);
+        System.out.println(myDouble);
+    }
+}
