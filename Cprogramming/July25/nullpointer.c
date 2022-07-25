0dc724012f06baabfbc1a43cea173f23f856c086
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-07-25 15:57:23 +0530

    null and void added

---
+++ Cprogramming/July25/nullpointer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int *p;//dereferencing operator
+    p = NULL;
+    printf("the value of pointer is %d",p);
+    return 0;
+}
+
+
+// op : the value of pointer is 0
